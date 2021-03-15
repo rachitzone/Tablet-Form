2d469422ffbc8ad8e72f174b56e74536ef74585d
Rachit Singh <72936223+rachitzone@users.noreply.github.com>
2021-03-15 16:38:12 +0530

    Delete Web Devlopment directory

--- Web Devlopment/js/animation.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$('li.fields').filter(':nth-child(n+4)').addClass('hide');
-	
-	$('ul').on('click','li.title',function(){
-		$(this).next().slideDown(200).siblings('li.fields').slideUp(200);
-	})
-});
